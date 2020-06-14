91723aea949d944e6dd2ff528acf9466dc429873
Michael Reynolds <reynoldsmichael945@gmail.com>
2020-06-14 15:09:16 -0400

    fetched satellite data from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,6 +20,7 @@ export class AppComponent {
         let satellite: object;
         // TODO: loop over satellites
         for (let i = 0; i < fetchedSatellites.length; i++) {
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
           satellite = new Satellite(
             fetchedSatellites[i]["name"],
             fetchedSatellites[i]["type"],
@@ -27,10 +28,9 @@ export class AppComponent {
             fetchedSatellites[i]["orbitType"],
             fetchedSatellites[i]["operational"]
           );
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           this.sourceList.push(satellite);
         }
-        // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-        // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
 
       }.bind(this));
     }.bind(this));
