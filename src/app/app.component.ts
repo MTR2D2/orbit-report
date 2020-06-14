e24a75ab5ca66cc61008ee81b124b6502a7b2bf4
Michael Reynolds <reynoldsmichael945@gmail.com>
2020-06-14 18:36:20 -0400

    Added a div tag in app.component.html with css search-form styling Added an input tag inside div Added keyup.enter handler inside input tag that calls search(searchTerm.value) Added a button inside div with click handler and also calls search(searchTerm.value) Added a search method to the AppComponent class Changed to [satellites]=displayList in app-orbit-list tag Added #searchTerm to input tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
@@ -30,11 +32,28 @@ export class AppComponent {
           );
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           this.sourceList.push(satellite);
+          //this.displayList.push(satellite);
+          // make a copy of the sourceList to be shown to the user
+          this.displayList = this.sourceList.slice(0);
         }
 
       }.bind(this));
     }.bind(this));
 
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
   }
 
 }
